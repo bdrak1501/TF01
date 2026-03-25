5eac462ec41ce8f5243c811906dc7277a1b2d571
bdrak1501 <bdrak1501@gmail.com>
2026-03-25 13:27:33 +0100

    fix backend better-sqlite

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -8,10 +7,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = new sqlite3.Database("shop.db");
+const Database = require("better-sqlite3");
+const db = new Database("shop.db");
 
 /* BAZA */
-db.run(`
+db.prepare(`
 CREATE TABLE IF NOT EXISTS orders (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT,
@@ -23,60 +23,52 @@ total INTEGER,
 status TEXT,
 date TEXT
 )
-`);
+`).run();
 
 /* NOWE ZAMÓWIENIE */
 app.post("/order", (req, res) => {
 
-const o = req.body;
+    const o = req.body;
 
-db.run(`
-INSERT INTO orders
-(name,email,phone,address,products,total,status,date)
-VALUES (?,?,?,?,?,?,?,?)
-`,
-[
-o.name,
-o.email,
-o.phone,
-o.address,
-JSON.stringify(o.products),
-o.total,
-"Nowe",
-new Date().toLocaleString()
-],
-function(err){
+    const stmt = db.prepare(`
+    INSERT INTO orders
+    (name,email,phone,address,products,total,status,date)
+    VALUES (?,?,?,?,?,?,?,?)
+    `);
 
-if(err){
-return res.status(500).send(err);
-}
+    const info = stmt.run(
+        o.name,
+        o.email,
+        o.phone,
+        o.address,
+        JSON.stringify(o.products),
+        o.total,
+        "Nowe",
+        new Date().toLocaleString()
+    );
 
-res.json({success:true,id:this.lastID});
-
-});
-
+    res.json({ success: true, id: info.lastInsertRowid });
 });
 
 /* POBIERZ */
-app.get("/orders", (req,res)=>{
+app.get("/orders", (req, res) => {
 
-db.all("SELECT * FROM orders ORDER BY id DESC", (err,rows)=>{
-res.json(rows);
-});
+    const rows = db.prepare("SELECT * FROM orders ORDER BY id DESC").all();
+    res.json(rows);
 
 });
 
 /* STATUS */
-app.post("/status", (req,res)=>{
+app.post("/status", (req, res) => {
 
-db.run(
-"UPDATE orders SET status=? WHERE id=?",
-[req.body.status, req.body.id],
-()=> res.json({success:true})
-);
+    db.prepare("UPDATE orders SET status=? WHERE id=?")
+    .run(req.body.status, req.body.id);
+
+    res.json({ success: true });
 
 });
 
+/* START */
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
